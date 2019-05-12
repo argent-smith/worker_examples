62c51f318b650afbb70e269a1bd7b5601b5a97f0
Pavel Argentov <argentoff@gmail.com>
2019-05-13 01:49:59 +0300

    Lwt version rewrite: we count file sizes
    * Also, no monadic syntax anymore

--- worker_lwt/src/bin/worker_lwt.ml
+++ worker_lwt/src/bin/worker_lwt.ml
@@ -1,37 +1,34 @@
-include Lwt.Infix
-
 let num_files = 10
 
-(* Infix AKA monadic syntax *)
 let reader n =
   let name = Printf.sprintf "files/%d.dat" n in
   let stream = Lwt_io.chars_of_file name in
   let rec read_stream chars i =
-    Lwt_stream.get chars
-    >>=
-      (function
-       | None -> Lwt_io.printlf "%s\t=> %d chars" name i
-       | _    -> read_stream chars (i + 1))
+    match%lwt Lwt_stream.get chars with
+    | None -> Lwt.return (name, i)
+    | _    -> read_stream chars (i + 1)
   in
   read_stream stream 0
 
-(* Lwt PPX syntax *)
-let reader' n =
-  let name = Printf.sprintf "files/%d.dat" n in
-  let stream = Lwt_io.chars_of_file name in
-  let rec read_stream chars i =
-    let%lwt () = Lwt_stream.junk chars in
-    let%lwt () = Lwt_io.printlf "%d\t=>\t#%d\t<=\t%s" n i name in
-    let%lwt () = Lwt_unix.sleep 0.001 in
-    read_stream chars (i + 1) in
-  read_stream stream 1
+let collect_results =
+  let rec append_to_ready ready pending =
+    let%lwt (ready', pending') = pending in
+    let ready'' = List.append ready ready' in
+    match List.length pending' with
+    | 0 -> Lwt.return ready''
+    | _ -> Lwt.nchoose_split pending'
+           |> append_to_ready ready''
+  in
+  append_to_ready []
 
-let spawn_readers_for num =
+let run_readers_for num =
+  let%lwt () = Lwt_io.printlf "Starting %d workers" num in
   List.init num (fun i -> i + 1)
   |> List.map reader
-  |> Lwt.join
+  |> Lwt.nchoose_split
+  |> collect_results
 
 let () =
-  Printf.printf "Starting %d workers\n" num_files;
-  flush stdout;
-  Lwt_main.run @@ spawn_readers_for num_files
+  run_readers_for num_files
+  |> Lwt_main.run
+  |> List.iter (fun (name, nchars) -> Printf.printf "%s\t=> %d chars\n" name nchars)
